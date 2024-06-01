07842187cfa245fbf4ca26ceaa339a11e15d2a40
vineetasin <singhvineeta2412@gmail.com>
2024-06-01 09:08:22 +0530

    just upload

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
+import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { IoIosClose } from "react-icons/io";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 function Card({ data, reference }) {
   return (
-    <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} dragElastic={.1}  className='relative flex-shrink-0 w-40 h-48 rounded-[35px] px-5 py-4 text-zinc-200/80 overflow-hidden bg-zinc-900/50'>
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.2 }}
+      dragElastic={0.1}
+      className="relative flex-shrink-0 w-48 h-60 rounded-[35px] px-5 py-4 text-zinc-200/80 overflow-hidden bg-zinc-900/80"
+    >
       <FaRegFileAlt />
-      <p className='text-[12px] leading-tight font-thin mt-3'>{data.desc}</p>
-      <div className='footer absolute bottom-0 w-full left-0'>
-        <div className='flex item-center justify-between px-4 mb-3'>
-          <h5 className='text-[11px]'>{data.filesize}</h5>
-          <span className='w-3 h-3 bg-zinc-600 rounded-full flex items-center justify-center'>
-            {data.close ? <IoIosClose /> : <LuDownload size=".5em" color='black' />}
+      <p className="text-[15px] leading-5 font-medium mt-5">{data.desc}</p>
+      <div className="footer absolute bottom-0 w-full left-0">
+        <div className="flex item-center justify-between px-4 mb-3">
+          <h5 className="text-[12px]">{data.filesize}</h5>
+          <span className="w-3 h-3 bg-zinc-600 rounded-full flex items-center justify-center">
+            {data.close ? (
+              <IoIosClose />
+            ) : (
+              <LuDownload size=".5em" color="black" />
+            )}
           </span>
         </div>
 
-        {
-          data.tag.isOpen && (
-            <div className={`tag w-full py-1 ${data.tag.tagColor === "purple" ? "bg-purple-600" : "bg-green-500" } flex items-center justify-center`}>
-              <h1 className='text-[10px] font-thin pb-2 mt-[4px]'>{data.tag.tagTitle}</h1>
-            </div>
-          ) }
-        
-
+        {data.tag.isOpen && (
+          <div
+            className={`tag w-full py-1 ${
+              data.tag.tagColor === "purple" ? "bg-purple-600" : "bg-green-500"
+            } flex items-center justify-center`}
+          >
+            <h1 className="text-[13px] font-semibold pb-1 mt-[4px]">
+              {data.tag.tagTitle}
+            </h1>
+          </div>
+        )}
       </div>
     </motion.div>
-
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,55 +1,56 @@
-import React, { useRef, useState } from 'react'
-import Card from './Card'
+import React, { useRef, useState } from "react";
+import Card from "./Card";
 
 function Forground() {
-
   const ref = useRef(null);
 
   const data = [
     {
-      desc: "Hare Krishna Hare Krishna, Krishna Krishna hare hare, hare Raam here Raam, Raam Raam Hare Hare",
+      desc: "There are no secrets to success. It is the result of preparation, hard work, and learning from failure.",
       filesize: ".4mb",
-      close: true ,
-      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green"}
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
     },
 
     {
-      desc: "Lorem ipsum dolor sigsdiuho",
+      desc: "Anyone who has never made a mistake has never tried anything new.",
       filesize: ".4mb",
-      close: true ,
-      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "purple"}
-    },  
-
-    {
-      desc: "Lorem ipsum dolor sigsdiuho",
-      filesize: ".4mb",
-      close: false ,
-      tag: { isOpen: false, tagTitle: "Upload", tagColor: "green"}
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "purple" },
     },
 
     {
-      desc: "Lorem ipsum dolor sigsdiuho",
+      desc: "Choose a job you love, and you will never have to work a day in your life.",
       filesize: ".4mb",
-      close: false ,
-      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue"}
+      close: false,
+      tag: { isOpen: false, tagTitle: "Upload", tagColor: "green" },
     },
 
     {
-      desc: "Lorem ipsum dolor sigsdiuho",
+      desc: "Creativity is not a talent. It is a way of operating",
       filesize: ".4mb",
-      close: true ,
-      tag: { isOpen: true, tagTitle: "Upload", tagColor: "purple"}
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
     },
 
+    {
+      desc: "The beginning is the most important part of the work.",
+      filesize: ".4mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "purple" },
+    },
   ];
 
   return (
-    <div ref={ref} className='fixed flex gap-10 top-0 p-4 flex-wrap left-0 z-[3] w-full h-full'>
+    <div
+      ref={ref}
+      className="fixed flex gap-10 top-0 p-4 flex-wrap left-0 z-[3] w-full h-full"
+    >
       {data.map((item, index) => (
         <Card data={item} reference={ref} />
       ))}
-     </div>
-  )
+    </div>
+  );
 }
 
-export default Forground
+export default Forground;
